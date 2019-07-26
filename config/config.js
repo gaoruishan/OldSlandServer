24aab972761d1d143f11ada8ebb6a2735626334d
gaoruishan <grs0515@163.com>
2019-07-26 09:35:25 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -5,19 +5,19 @@
 module.exports = {
     env: 'dev',
     database: {
-        dbName: 'gaoruishan_cn',
-        host: '148.70.145.170',
+        dbName: '',
+        host: '',
         port: '3306',
         user: 'root',
-        password: 'grs7412369852'
+        password: 'grs'
     },
     security:{
         secretKey:"abcdefg",
         expiresIn:60*60*24*30
     },
     wx:{
-        appId:'wx4b2c0ab903da2982',
-        appSecret:'9c818f88aed65af01b7e361b1fd3ed78',
+        appId:'wx4',
+        appSecret:'9c818f',
         loginUrl:'https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code'
     },
     yushu:{
